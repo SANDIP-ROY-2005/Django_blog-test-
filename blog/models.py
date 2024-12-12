a4a55f371fcbf227bdd4ead59072cf057f88d50b
SANDIP-ROY-2005 <161707774+SANDIP-ROY-2005@users.noreply.github.com>
2024-12-12 18:06:54 +0530

    forms.py issue fixed

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model):
+class Blog(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    description = models.TextField()
+    picture = models.ImageField(upload_to='blog_pictures/', null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
